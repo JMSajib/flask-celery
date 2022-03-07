5ecac3936d7b7cf8ab7eba152f6d52960e97c65f
JMSajib <jm.sajib012@gmail.com>
2022-03-07 21:46:48 +0600

    Added Application Factory Pattern

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
-import time
-from flask import Flask
-from celery import Celery
+from project import create_app, ext_celery
+
+app = create_app()
+celery = ext_celery.celery
+
+# import time
+# from flask import Flask
+# from celery import Celery
 
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-celery = Celery(
-    __name__,
-    broker="redis://127.0.0.1:6379/0",
-    backend="redis://127.0.0.1:6379/0"
-)
+# celery = Celery(
+#     __name__,
+#     broker="redis://127.0.0.1:6379/0",
+#     backend="redis://127.0.0.1:6379/0"
+# )
 
-@app.route('/')
-def hello():
-    return "Hello World"
+# @app.route('/')
+# def hello():
+#     return "Hello World"
 
-@celery.task
-def divide(x, y):
-    time.sleep(3)
-    return x / y
+# @celery.task
+# def divide(x, y):
+#     time.sleep(3)
+#     return x / y
